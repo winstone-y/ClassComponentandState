4c1fe0161cff82b00938c847a4001f5385eb5c36
Winstone <winstonwork57@gmail.com>
2023-06-07 14:30:11 +0000

    solved

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -4,24 +4,29 @@ import {Component} from 'react'
 import './index.css'
 
 class Welcome extends Component {
-  state = {isSubscribed: false}
+  state = {isSubscribed: false, buttonText: 'Subscribe'}
+
+  onSubscribe = () => {
+    const {isSubscribed, buttonText} = this.state
+    if (isSubscribed === false) {
+      this.setState({isSubscribed: true, buttonText: 'Subscribed'})
+      return buttonText
+    }
+    this.setState({isSubscribed: false, buttonText: 'Subscribe'})
+    return buttonText
+  }
 
   render() {
-    const {isSubscribed} = this.state
+    const {buttonText} = this.state
+
     return (
       <div className="bg-container">
         <h1 className="heading">Welcome</h1>
         <p className="tag-line">Thankyou, Happy Learning!</p>
         <div>
-          {isSubscribed ? (
-            <button type="button" className="button">
-              Subscribed
-            </button>
-          ) : (
-            <button type="button" className="button">
-              Subscribe
-            </button>
-          )}
+          <button className="button" type="button" onClick={this.onSubscribe}>
+            {buttonText}
+          </button>
         </div>
       </div>
     )
